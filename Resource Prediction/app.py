cc6a2b56d215a559f92df368d93dc90a80149596
Sanjay H <153520374+sanjayh-2022@users.noreply.github.com>
2024-08-11 10:25:29 +0530

    Update app.py

--- Resource Prediction/app.py
+++ Resource Prediction/app.py
@@ -62,4 +62,5 @@ def _build_cors_prelight_response():
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
