ede452a61b11db68e18479e347e5a0c312443a10
Sanjay H <153520374+sanjayh-2022@users.noreply.github.com>
2024-08-11 13:41:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS  # Import CORS from flask_cors
 from model import predict_resource_needs  # Import the function from model.py
 
 app = Flask(__name__)
-CORS(app, resources={r"/predict": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000", "supports_credentials": True}})
 
 @app.route('/predict', methods=['POST', 'OPTIONS'])  # Allow OPTIONS method
 def predict():
